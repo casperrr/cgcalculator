7eafbd0d7d3deb418c501595473667c905f19e50
casperrr <casper.sebastien@gmail.com>
2023-07-22 15:08:14 +0100

    added a dropdown component (not functional)

---
+++ src/components/Dropdown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Option.css';
+
+class Dropdown extends React.Component {
+    render(){
+        
+        return(
+            <div className='dropdown_container'>
+                <p>This is a Dropdown</p>
+            </div>
+        )
+    }
+}
+
+export {Dropdown};
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Option.css';
+import {Dropdown} from './Dropdown.js';
 
 class Option extends React.Component {
     render(){
         return(
             <div className='option_container'>
                 <p>{this.props.note ? this.props.note : "Option with undefined name"}</p>
+                <Dropdown></Dropdown>
             </div>
         )
     }
